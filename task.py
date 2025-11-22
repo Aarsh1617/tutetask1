4d18504de56ec0865716c46b4a5e623106bf927b
Aarsh1617 <aarynjoura@gmail.com>
2025-11-22 19:14:49 +0530

    add task.py file

---
+++ task.py
@@ -0,0 +1,14 @@
+a = float(input("Enter the first number : "))
+b = float(input("Enter the second number : "))
+
+add = a + b
+sub = a - b
+mul = a * b
+div = a / b
+
+print("The addition of the two numbers is : ",add)
+print("The substraction of the two numbers is : ",sub)
+print("The product of the two numbers is : ",mul)
+print("The division of the two numbers is : ",div)
+
+
